07189888afbdfb860a758c5d751c158a59ba6c4e
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-09-30 18:48:47 -0400

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -33,7 +33,7 @@ def make_sidebar():
         elif get_current_page_name() != "streamlit_app":
             # If anyone tries to access a secret page without being logged in,
             # redirect them to the login page
-            #st.write("Forbidden")
+            st.write("Forbidden")
             #st.switch_page("streamlit_app.py")
 
 
