3c0be12be96c465c546ccdfa8d7aacd4b6bcc197
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-09-30 18:11:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -75,7 +75,7 @@ if 'access_token' in st.session_state and 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = True
         st.write("You are in the guild! Access granted.")
         sleep(2)
-        st.markdown('[Go to App](./page1)')
+        st.markdown('[Go to App](/page1)')
         #st.switch_page("pages/page2.py")
         #st.markdown('[Go to App](https://psds-elite.streamlit.app/page1)')
     else:
