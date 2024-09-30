88fee0d9697c929fc03de2bbca625a928f7f893d
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-09-30 18:45:39 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -2,16 +2,18 @@ from navigation import make_sidebar
 import streamlit as st
 
 make_sidebar()
-
-st.write(
+if st.session_state.get('logged_in', False):
+    st.write(
+        """
+    # 🔓 Secret Company Stuff
+    
+    This is a secret page that only logged-in users can see.
+    
+    Don't tell anyone.
+    
+    For real.
+    
     """
-# 🔓 Secret Company Stuff
-
-This is a secret page that only logged-in users can see.
-
-Don't tell anyone.
-
-For real.
-
-"""
-)
+    )
+if not st.session_state.get('logged_in', False):
+    st.write("Forbidden")
