e2b00da39c36a26d0ace89b887c777e368aec134
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-04 12:13:53 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,19 +1,13 @@
 from navigation import make_sidebar
 import streamlit as st
+from st_files_connection import FilesConnection
+
+#df1 = st.empty()
 
 make_sidebar()
 if st.session_state.get('logged_in', False):
-    st.write(
-        """
-    # 🔓 Secret Company Stuff
-    
-    This is a secret page that only logged-in users can see.
-    
-    Don't tell anyone.
-    
-    For real.
-    
-    """
-    )
+    conn = st.connection('gcs', type=FilesConnection)
+    df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=600)
+    df1.dataframe(df, hide_index=True)
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")
