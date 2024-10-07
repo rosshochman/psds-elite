d4322ac83799d0eb750e4a83d3ad99c5686677df
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-07 18:26:15 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -14,11 +14,19 @@ if st.session_state.get('logged_in', False):
     st.markdown("Data below is for all 13D/G filings for small cap tickers. Please use the MultiSelect tools to filter for your search criteria.")
     col1, col2 = st.columns(2)
     if 'ticker' in df.columns:
+        df['ticker'] = df['ticker'].astype(str)
         unique_tickers = sorted(set(df['ticker']))
         with col1:
             selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
         if selected_tickers:
             df = df[df['ticker'].isin(selected_tickers)]
+    if 'formType' in df.columns:
+        df['formType'] = df['formType'].astype(str)
+        unique_form = sorted(set(df['formType']))
+        with col1:
+            selected_form = st.multiselect('Select Form Type:', options=unique_form)
+        if selected_form:
+            df = df[df['formType'].isin(selected_form)]
     if 'owners1.name' in df.columns:
         df['owners1.name'] = df['owners1.name'].astype(str)
         unique_owners = sorted(set(df['owners1.name']))
