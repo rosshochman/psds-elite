de9f6f7ee08f73304b592b50a0908ee49fe0e6aa
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-07 16:05:16 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -12,14 +12,16 @@ df = conn.read("psds_streamlit/13G_13D_data.csv", input_format="csv", ttl=3600)
 make_sidebar()
 if st.session_state.get('logged_in', False):
     st.subheader("Filter by Ticker")
-    unique_tickers = sorted(set(df['ticker']))
-    selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
-    if selected_tickers:
-        df = df[df['ticker'].isin(selected_tickers)]
-    unique_owners = sorted(set(df['owners1.name']))
-    selected_owners = st.multiselect('Select Owners:', options=unique_owners)
-    if selected_owners:
-        df = df[df['owners1.name'].isin(selected_owners)]
+    if 'ticker' in df.columns:
+        unique_tickers = sorted(set(df['ticker']))
+        selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
+        if selected_tickers:
+            df = df[df['ticker'].isin(selected_tickers)]
+    if 'owners1.name' in df.columns:
+        unique_owners = sorted(set(df['owners1.name']))
+        selected_owners = st.multiselect('Select Owners:', options=unique_owners)
+        if selected_owners:
+            df = df[df['owners1.name'].isin(selected_owners)]
     df1 = st.empty()
     df1.dataframe(df,use_container_width=True, hide_index=True)
 
