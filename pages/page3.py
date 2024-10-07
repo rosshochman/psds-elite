e550a2faf75b1d787a141300ebd22e4a184da2e6
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-07 16:11:15 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -7,20 +7,22 @@ st.set_page_config(layout="wide")
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("psds_streamlit/13G_13D_data.csv", input_format="csv", ttl=3600)
 
-
+col1, col2 = st.columns(2)
 
 make_sidebar()
 if st.session_state.get('logged_in', False):
     st.subheader("Filter by Ticker")
     if 'ticker' in df.columns:
         unique_tickers = sorted(set(df['ticker']))
-        selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
+        with col1:
+            selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
         if selected_tickers:
             df = df[df['ticker'].isin(selected_tickers)]
     if 'owners1.name' in df.columns:
         df['owners1.name'] = df['owners1.name'].astype(str)
         unique_owners = sorted(set(df['owners1.name']))
-        selected_owners = st.multiselect('Select Owners:', options=unique_owners)
+        with col2:
+            selected_owners = st.multiselect('Select Owners:', options=unique_owners)
         if selected_owners:
             df = df[df['owners1.name'].isin(selected_owners)]
     df1 = st.empty()
