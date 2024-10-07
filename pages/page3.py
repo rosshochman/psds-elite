4c18d091acb4378f35746e834d77c2ff5654bf7c
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-07 15:49:08 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -12,6 +12,9 @@ df = conn.read("psds_streamlit/13G_13D_data.csv", input_format="csv", ttl=3600)
 make_sidebar()
 if st.session_state.get('logged_in', False):
     df1 = st.empty()
+    st.subheader("Filter by Ticker")
+    unique_tickers = sorted(set([ticker for sublist in df['Ticker'] for ticker in sublist]))
+    selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
     df1.dataframe(df,use_container_width=True, hide_index=True)
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")
