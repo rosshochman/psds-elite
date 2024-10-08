605b11d7c88e1f1590450c064867df1ab00923cc
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-08 10:09:10 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -11,6 +11,29 @@ df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=
 
 make_sidebar()
 if st.session_state.get('logged_in', False):
+    st.markdown("Data below is for all small cap tickers. Please use the MultiSelect tools to filter for your search criteria.")
+    col1, col2, col3= st.columns(3)
+    if 'Ticker' in df.columns:
+        df['Ticker'] = df['Ticker'].astype(str)
+        unique_tickers = sorted(set(df['ticker']))
+        with col1:
+            selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
+        if selected_tickers:
+            df = df[df['Ticker'].isin(selected_tickers)]
+    if 'Sector' in df.columns:
+        df['Sector'] = df['Sector'].astype(str)
+        unique_sector = sorted(set(df['Sector']))
+        with col2:
+            selected_sector = st.multiselect('Select Sector:', options=unique_sector)
+        if selected_sector:
+            df = df[df['Sector'].isin(selected_sector)]
+    if 'Industry' in df.columns:
+        df['Industry'] = df['Industry'].astype(str)
+        unique_industry = sorted(set(df['Industry']))
+        with col3:
+            selected_indsutry = st.multiselect('Select Industry:', options=unique_industry)
+        if selected_indsutry:
+            df = df[df['Industry'].isin(selected_indsutry)]
     df1 = st.empty()
     df1.dataframe(df, column_config={"Website": st.column_config.LinkColumn("Website"),
                                      "Description":st.column_config.Column(width="medium"),
