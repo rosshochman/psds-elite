392673964cc0950d04ebe4250854b4bef626a1e8
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-09 11:45:06 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -12,6 +12,7 @@ df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=
 
 make_sidebar()
 if st.session_state.get('logged_in', False):
+    search_text = ""
     st.markdown("Data below is for all small cap tickers. Please use the MultiSelect tools to filter for your search criteria.")
     col1, col2, col3, col4= st.columns(4)
     if 'Ticker' in df.columns:
