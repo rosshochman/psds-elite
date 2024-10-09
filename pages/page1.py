f4c50220ce7e3035a683fde1810bc647291927f9
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-09 13:45:47 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -57,7 +57,6 @@ if st.session_state.get('logged_in', False):
             search_text = st.text_input("Enter text to search in the Description column:")
             if 'session_search_string' not in st.session_state:
                 st.session_state['session_search_string'] = "No search in progress."
-            st.markdown(st.session_state['session_search_string'])
             sub_col1, sub_col2= st.columns(2)
             with sub_col1:
                 if st.button("Search", use_container_width=True):
@@ -66,6 +65,7 @@ if st.session_state.get('logged_in', False):
                         df = df[df['Description'].str.contains(search_text, case=False, na=False)]
                         df = df
                         st.session_state['session_search_string'] = "Currently searching for "+search_text
+                        st.markdown(st.session_state['session_search_string'])
                         #st.rerun()
                         #st.session_state['filtered_df'] = df
                     else:
@@ -73,6 +73,7 @@ if st.session_state.get('logged_in', False):
             with sub_col2:
                 if st.button("Reset", use_container_width=True):
                     st.session_state['session_search_string'] = "No search in progress."
+                    st.markdown(st.session_state['session_search_string'])
                     #st.rerun()
                     df = df
 
