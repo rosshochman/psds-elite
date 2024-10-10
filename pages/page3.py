0eac20bc9619a3174e4da74082319f7fd60e8538
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-10 17:13:45 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -40,8 +40,10 @@ if st.session_state.get('logged_in', False):
         if selected_owners:
             df = df[df['Owners'].apply(lambda x: any(term.lower() in x.lower() for term in selected_owners))]
     df1 = st.empty()
-    df1.dataframe(df, column_config={"HTML Link": st.column_config.LinkColumn("HTML Link", width="small"),
+    df1.dataframe(df, column_config={"Folder Link": st.column_config.LinkColumn("Folder Link", width="small"),
                                      "Place":st.column_config.Column(width="small"),
+                                     "Amount Excludes Certain Shares":st.column_config.Column(width="small"),
+                                     "Schedule 13G Filed Previously":st.column_config.Column(width="small"),
                                      "Filing Link": st.column_config.LinkColumn("Filing Link", width="small")},
                                      use_container_width=True, hide_index=True, height=750)
 
