0b3d48f5105c95d0e8bb192ba603ac5da7665f7c
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-10 16:48:22 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -18,20 +18,20 @@ make_sidebar()
 if st.session_state.get('logged_in', False):
     st.markdown("Data below is for all 13D/G filings for small cap tickers. Please use the MultiSelect tools to filter for your search criteria.")
     col1, col2, col3= st.columns(3)
-    if 'ticker' in df.columns:
-        df['ticker'] = df['ticker'].astype(str)
-        unique_tickers = sorted(set(df['ticker']))
+    if 'Ticker' in df.columns:
+        df['Ticker'] = df['Ticker'].astype(str)
+        unique_tickers = sorted(set(df['Ticker']))
         with col1:
             selected_tickers = st.multiselect('Select Tickers:', options=unique_tickers)
         if selected_tickers:
-            df = df[df['ticker'].isin(selected_tickers)]
-    if 'formType' in df.columns:
-        df['formType'] = df['formType'].astype(str)
-        unique_form = sorted(set(df['formType']))
+            df = df[df['Ticker'].isin(selected_tickers)]
+    if 'Form Type' in df.columns:
+        df['Form Type'] = df['Form Type'].astype(str)
+        unique_form = sorted(set(df['Form Type']))
         with col2:
             selected_form = st.multiselect('Select Form Type:', options=unique_form)
         if selected_form:
-            df = df[df['formType'].isin(selected_form)]
+            df = df[df['Form Type'].isin(selected_form)]
     if 'Owners' in df.columns:
         df['Owners'] = df['Owners'].astype(str)
         unique_owners = sorted(set(owner.strip() for owners_list in df['Owners'].str.split('|') for owner in owners_list if owner.strip()))
