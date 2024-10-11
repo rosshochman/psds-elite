e006fce34bb8aa11c0c7a1d6b0168690a9fbf582
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-11 16:23:27 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -63,7 +63,14 @@ if st.session_state.get('logged_in', False):
         df = df[df['Industry'].isin(st.session_state['selected_industry'])]
     
     # Display filtered DataFrame
-    st.dataframe(df, use_container_width=True, height=750)
+
+    df1 = st.empty()
+    df1.dataframe(df, column_config={"Website": st.column_config.LinkColumn("Website"),
+                                     "Description":st.column_config.Column(width="medium"),
+                                     "Name":st.column_config.Column(width="medium"),
+                                    "Sector":st.column_config.Column(width="medium"),
+                                    "Industry":st.column_config.Column(width="medium")}, use_container_width=True, hide_index=True, height=750)
+
 
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")
