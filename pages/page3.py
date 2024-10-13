27b916f3e458c81e71c17af77f96f00b2eda9c82
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-13 18:49:17 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -5,7 +5,7 @@ import re
 import pandas as pd
 
 st.set_page_config(layout="wide")
-
+#test
 conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("psds_streamlit/13G_13D_data.csv", input_format="csv", ttl=3600)
 df['All Owners'] = df['All Owners'].fillna('')
