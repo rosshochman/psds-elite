884e28ec0287a7a6f6930320fcf6f1a097825c70
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-13 18:41:28 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -5,10 +5,9 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 conn = st.connection('gcs', type=FilesConnection)
-#df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=3600)
-df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=3600, na_values=['', 'NaN'], keep_default_na=True)
+df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=3600)
 # Check if "NA" is present in the 'Ticker' column before replacing it
-#df['Ticker'] = df['Ticker'].fillna('NA')
+df['Ticker'] = df['Ticker'].fillna('NA')
 
 #df['MarketCap'] = df['MarketCap'].fillna(0)
 #df['Float'] = df['MarketCap'].fillna(0)
