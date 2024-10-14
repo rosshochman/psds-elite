69c77c89fa07c53e8602805159f7163bb3eb7b41
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-14 13:45:03 -0400

    Update page8.py

--- pages/page8.py
+++ pages/page8.py
@@ -1,1 +1,13 @@
-
+from navigation import make_sidebar
+import streamlit as st
+st.set_page_config(layout="wide")
+make_sidebar()
+if st.session_state.get('logged_in', False):
+    st.write(
+        """
+Catalyst Tracker Coming Soon
+    
+    """
+    )
+if not st.session_state.get('logged_in', False):
+    st.write("Forbidden")
