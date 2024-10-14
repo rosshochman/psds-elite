6383f523bfa14d0a6647904775aa3dbace8042c7
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-14 13:44:51 -0400

    Update page7.py

--- pages/page7.py
+++ pages/page7.py
@@ -1,2 +1,14 @@
-#ticker look up
+from navigation import make_sidebar
+import streamlit as st
+st.set_page_config(layout="wide")
+make_sidebar()
+if st.session_state.get('logged_in', False):
+    st.write(
+        """
+Ticker Lookup Coming Soon
+    
+    """
+    )
+if not st.session_state.get('logged_in', False):
+    st.write("Forbidden")#ticker look up
 #single select box to select ticker - then populate data frames and charts after selected
