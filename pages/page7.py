3aa15284c6c6e6785195db125fef4f34b9733640
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-16 15:36:30 -0400

    Update page7.py

--- pages/page7.py
+++ pages/page7.py
@@ -1,9 +1,20 @@
 from navigation import make_sidebar
 import streamlit as st
+from st_files_connection import FilesConnection
+import pandas as pd
+
+st.set_page_config(layout="wide")
+conn = st.connection('gcs', type=FilesConnection)
+df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=3600)
+# Check if "NA" is present in the 'Ticker' column before replacing it
+df['Ticker'] = df['Ticker'].fillna('NA')
+
+
 st.set_page_config(layout="wide")
 make_sidebar()
 if st.session_state.get('logged_in', False):
     st.write("""Ticker Lookup Coming Soon""")
+    selected_tickers = st.selectbox('Select Ticker:', df['Ticker'].unique())
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")#ticker look up
 #single select box to select ticker - then populate data frames and charts after selected
