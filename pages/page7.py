05fb30cfbf74e52c2cb205707db2dd64f42e06d0
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-16 15:40:29 -0400

    Update page7.py

--- pages/page7.py
+++ pages/page7.py
@@ -8,11 +8,17 @@ conn = st.connection('gcs', type=FilesConnection)
 df = conn.read("psds_streamlit/uploaded-data_test.csv", input_format="csv", ttl=3600)
 # Check if "NA" is present in the 'Ticker' column before replacing it
 df['Ticker'] = df['Ticker'].fillna('NA')
+ticker_options = ['Select a Ticker'] + list(df['Ticker'].unique())
+
 
 make_sidebar()
 if st.session_state.get('logged_in', False):
     st.write("""Ticker Lookup Coming Soon""")
-    selected_tickers = st.selectbox('Select Ticker:', df['Ticker'].unique())
+    selected_tickers = st.selectbox('Select Ticker:', ticker_options)
+    if selected_ticker != 'Select a Ticker':
+        st.write(f'You selected: {selected_ticker}')
+    else:
+        st.write('Please select a Ticker')
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")#ticker look up
 #single select box to select ticker - then populate data frames and charts after selected
