a0988651b7dcdf3c69fa8707e4dd596f43649dda
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-22 17:36:10 -0400

    Update page6.py

--- pages/page6.py
+++ pages/page6.py
@@ -1,13 +1,18 @@
 from navigation import make_sidebar
 import streamlit as st
+from st_files_connection import FilesConnection
+import pandas as pd
+
+conn = st.connection('gcs', type=FilesConnection)
+df_keyword = conn.read("psds_streamlit/full_text_final.csv", input_format="csv", ttl=3600)
+
 st.set_page_config(layout="wide")
+
+
 make_sidebar()
 if st.session_state.get('logged_in', False):
-    st.write(
-        """
-Keyword Tracker Coming Soon
-    
-    """
-    )
+    st.write("""Keyword Tracker Coming Soon""")
+    df1 = st.empty()
+    df1.dataframe(df_keyword, use_container_width=True, hide_index=True)
 if not st.session_state.get('logged_in', False):
     st.write("Forbidden")
