7db8d11670ce78fac9d8cbdc4af57c7419a937a9
Ross Hochman <38264792+rosshochman@users.noreply.github.com>
2024-10-23 15:30:19 -0400

    Update page7.py

--- pages/page7.py
+++ pages/page7.py
@@ -35,7 +35,7 @@ if st.session_state.get('logged_in', False):
         df1 = st.empty()
         df1.dataframe(transposed_df, use_container_width=True, hide_index=True)
         filtered_keyword_df = df_keyword_grouped[df_keyword_grouped['Ticker'] == selected_ticker]
-        st.write("""Keyword Data Below""")
+        st.write("""Keyword Data""")
         df2 = st.empty()
         df2.dataframe(filtered_keyword_df, use_container_width=True, hide_index=True)
     else:
